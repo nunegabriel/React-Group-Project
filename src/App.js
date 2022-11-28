2da211225018e82ccf34af9a5f0d78c6cdabac79
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-11-28 14:47:43 +0100

    Add react-redux Dependecies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/my-profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
