db865e24d74b7789ea74f804088bdad08724d6d6
nunegabriel <nune.gabriel.gn@gmail.com>
2022-11-28 16:51:55 +0000

    Profile Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Profiles from './components/Profiles';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter />
+      <Profiles />
     </div>
   );
 }
