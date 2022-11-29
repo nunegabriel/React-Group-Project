b24c34ffb15f9dc126b04c337354b10543513a32
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-11-29 10:29:26 +0100

    Install axios and react-thunk and fetch rockets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Rockets from './components/rockets/Rockets';
 
 function App() {
   return (
@@ -9,9 +11,9 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/my-profile" element={<Profile />} /> */}
+          <Route path="/" element={<Rockets />} />
+          {/* <Route path="/missions" element={<Missions />} /> */}
+          {/* <Routes path="/my-profile" element={<Profile />} />  */}
         </Routes>
       </BrowserRouter>
     </div>
