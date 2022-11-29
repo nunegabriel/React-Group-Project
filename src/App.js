bc43e7d04b13b00119fea7cd06d2a86039dde39e
nunegabriel <nune.gabriel.gn@gmail.com>
2022-11-29 10:41:24 +0000

    Add Mission

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Profiles from './components/Profiles';
+import Missions from './components/Missions';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -10,9 +11,8 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Rockets />} />
+          {/* <Route path="/" element={<Rockets />} /> */}
           <Route path="/missions" element={<Missions />} />
-          <Route path="/my-profile" element={<Profiles />} /> */}
           <Route path="/my-profile" element={<Profiles />} />
         </Routes>
         {/* <Profiles /> */}
