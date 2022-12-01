98e9393454349a80ad916aad3bf21ca7dd6335d4
nunegabriel <nune.gabriel.gn@gmail.com>
2022-12-01 10:07:24 +0000

    ADD MISSION LEAVING AND JOINING

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,44 +1,66 @@
 /* eslint-disable */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getPosts } from '../redux/missions/missionSlice'
+import { joinMission, fetchMissions, leaveMission } from '../redux/missions/missionSlice'
 
 function Missions() {
 
-  const { posts, loading } = useSelector((state) => state.post);
-
+  const missions = useSelector((state) => state.missions);
   const dispatch = useDispatch();
+  const initMission = [];
+  
   useEffect(() => {
-    dispatch(getPosts());
-  })
+    if ((missions.length || initMission.length) === 0) {
+      dispatch(fetchMissions());
+    }
+  }, [dispatch, missions]);
 
+
+  const bookMission = (mission) => {
+    if (!mission.reserved) {
+      dispatch(joinMission(mission.id));
+    } else {
+      dispatch(leaveMission(mission.id));
+    }
+  };
   return (
-       <div className="missions">
+    <div className="missions">
       <table>
         <thead>
-        <tr className="row">
+          <tr className="row">
             <th className="column head">Mission</th>
             <th className="column head">Description</th>
             <th className="column head">Status</th>
             <th className="column head">{' '}</th>
           </tr>
         </thead>
-        
+
         <tbody>
-        {posts.map((item) => (
-          <tr>
-            <td>{item.mission_name}</td>
-            <td className="desc">{item.description}</td>
-            <td><button type="submit">Not A member</button></td>
-            <td><button type="submit">Join Mission</button></td>
-          </tr>
-        ))}
+          {missions.map((mission) => (
+            <tr key={mission.id}>
+              <td>{mission.mission_name}</td>
+              <td className="desc">{mission.description}</td>
+              <td>
+                <div className="mission-status">
+                  {mission.reserved && 'Active Member'}
+                  {!mission.reserved && 'Not a Member'}
+                </div>
+              </td>
+              <td><button key={mission.id} type="submit"
+              onClick={() => bookMission(mission)}
+              >
+                 {mission.reserved && 'Leave Mission'}
+                {!mission.reserved && 'Join Mission'}
+              </button>
+              </td>
+            </tr>
+          ))}
 
         </tbody>
 
-        </table>
-   
-</div>
+      </table>
+
+    </div>
   );
 }
 
