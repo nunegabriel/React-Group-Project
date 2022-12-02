a28c361f32619cd0c04a88c15a10f411aab43ee0
nunegabriel <nune.gabriel.gn@gmail.com>
2022-12-02 10:55:47 +0000

    Add changes

--- src/components/__tests__/Missions.test.js
+++ src/components/__tests__/Missions.test.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
+
 import store from '../../redux/configureStore';
 import Missions from '../Missions';
 
