4ab885a1b28fc9adbc7d463636aaeeb79fc19778
nunegabriel <nune.gabriel.gn@gmail.com>
2022-12-02 11:30:04 +0000

    Add changes

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { joinMission, fetchMissions, leaveMission } from '../redux/missions/missionSlice'
 
 function Missions() {
-
   const missions = useSelector((state) => state.missions);
   const dispatch = useDispatch();
   const initMission = [];
